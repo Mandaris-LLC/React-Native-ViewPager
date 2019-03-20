425825136bbf9e775834bdd738d411aeb95d4541
Alexander Engel <ae94@icloud.com>
2019-03-20 11:16:27 +0100

    fixed key prop

--- viewpager/indicator/PagerTitleIndicator.js
+++ viewpager/indicator/PagerTitleIndicator.js
@@ -95,11 +95,10 @@ export default class PagerTitleIndicator extends Component {
                 spacingView = <View style={{ width: this.props.spacing }} />
             }
             return (
-                <>
+                <View key={index}>
                     <TouchableOpacity
                         style={[styles.titleContainer, itemMarginObj, itemStyle, isSelected ? selectedItemStyle : {}]}
                         activeOpacity={0.6}
-                        key={index}
                         onLayout={e => {
                             itemLayoutInfo[index] = e.nativeEvent;
                         }}
@@ -115,7 +114,7 @@ export default class PagerTitleIndicator extends Component {
                         {isSelected ? <View style={[styles.selectedBorder, selectedBorderStyle]} /> : null}
                     </TouchableOpacity>
                     {spacingView}
-                </>
+                </View>
             )
         })
         return (
